e98b8a6f4f5a80cf90c850d74649721234b97f66
VolodiaPG <55993206+VolodiaPG@users.noreply.github.com>
2019-10-08 21:58:16 +0200

    Updated Board
    completed methods and added enum

--- src/Board.cpp
+++ src/Board.cpp
@@ -4,6 +4,13 @@ namespace mcts
 {
 Board::Board()
 {
+    for (int ii = 0; ii < (int)boardValues.size(); ++ii)
+    {
+        for (int jj = 0; jj < (int)boardValues[ii].size(); ++jj)
+        {
+            boardValues[ii][jj] = 0;
+        }
+    }
 }
 
 void Board::performMove(int player, Position pos)
@@ -14,22 +21,25 @@ void Board::performMove(int player, Position pos)
 
 int Board::checkForWin(boardLine &line)
 {
-    int previous = IN_PROGRESS;
+    int previous = line[0];
+
+    // check if this is the same value all along the line
     for (int &value : line)
     {
         if (value != previous)
         {
-            //line is not full of the same values
+            // line is not full of the same values
             return 0;
         }
     }
 
-    //it has been the same value all along from the beginning
+    // it has been the same value all along from the beginning
     return previous;
 }
 
-game_status Board::checkStatus()
+GameStatus Board::checkStatus()
 {
+    // declare the two diags
     boardLine diag1 = boardLine();
     boardLine diag2 = boardLine();
 
@@ -45,44 +55,58 @@ game_status Board::checkStatus()
             col[ii] = boardValues[jj][ii];
         }
 
-        //row
+        // row
         win = checkForWin(row);
         if (win)
         {
-            return (game_status)win;
+            return (GameStatus)win;
         }
 
-        //col
+        // col
         win = checkForWin(col);
         if (win)
         {
-            return (game_status)win;
+            return (GameStatus)win;
         }
 
-        //2 big diagonals
+        // filling the 2 big diagonals
         diag1[ii] = boardValues[ii][ii];
         diag2[ii] = boardValues[ii][BOARD_SIZE - 1 - ii];
     }
 
-    //diag1
+    // diag1
     win = checkForWin(diag1);
     if (win)
     {
-        return (game_status)win;
+        return (GameStatus)win;
     }
 
-    //diag2
+    // diag2
     win = checkForWin(diag2);
     if (win)
     {
-        return (game_status)win;
+        return (GameStatus)win;
     }
 
     return getEmptyPositions()->size() > 0 ? IN_PROGRESS : DRAW;
 }
 
-std::unique_ptr<std::list<Position>> Board::getEmptyPositions()
+std::shared_ptr<std::list<Position>> Board::getEmptyPositions()
 {
+    std::shared_ptr<std::list<Position>> ret = std::make_shared<std::list<Position>>();
+
+     for (int ii = 0; ii < (int)boardValues.size(); ++ii)
+    {
+        for (int jj = 0; jj < (int)boardValues[ii].size(); ++jj)
+        {
+            if (boardValues[ii][jj])
+            {
+               ret->push_back(Position(ii, jj));
+            }
+        }
+    }
+
+    return ret;
 }
 
 } // namespace mcts
--- src/Board.hpp
+++ src/Board.hpp
@@ -11,29 +11,64 @@
 namespace mcts
 {
 
+/**
+ * @brief Describe possibles states of the game
+ * 
+ */
+typedef enum
+{
+    /**
+     * @brief Game in progress
+     * 
+     */
+    IN_PROGRESS = 0,
+    /**
+     * @brief Draw
+     * 
+     */
+    DRAW,
+    /**
+     * @brief P1's turn
+     * 
+     */
+    P1,
+    /**
+     * @brief P2's turn
+     * 
+     */
+    P2
+} GameStatus;
+
 using boardLine = std::array<int, BOARD_SIZE>;
 using boardMatrix = std::array<boardLine, BOARD_SIZE>;
 
-class Board : AbstractBoard
+class Board : AbstractBoard<GameStatus>
 {
 protected:
     boardMatrix boardValues = boardMatrix();
     int totalMoves = 0;
 
     /**
- * @brief Check a line (col or row) for a win
- * 
- * @param line array: col or row or diag
- * @return int the player who won, 0 if nobody has won at the call
- */
+    * @brief Check a line (col or row) for a win
+    * 
+    * @param line array: col or row or diag
+    * @return int the player who won, 0 if nobody has won at the call  
+    */
     int checkForWin(boardLine &line);
 
 public:
+    /**
+    * @brief Construct a new Board object
+    * 
+    */
     Board();
-    void performMove(int player, Position pos);
-    game_status checkStatus();
-    std::unique_ptr<std::list<Position>> getEmptyPositions();
+
+    //inherited from AbstractBoard
+    void performMove(int player, Position pos) override;
+    GameStatus checkStatus() override;
+    std::shared_ptr<std::list<Position>> getEmptyPositions() override;
 };
+
 } // namespace mcts
 
 #endif
