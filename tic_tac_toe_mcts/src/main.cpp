cd2736bbf848dad4196a974fe42e07e0b53b0d64
VolodiaPG <55993206+VolodiaPG@users.noreply.github.com>
2019-11-26 21:13:58 +0000

    testing callback between c++ & JS

--- tic_tac_toe_mcts/src/main.cpp
+++ tic_tac_toe_mcts/src/main.cpp
@@ -2,9 +2,23 @@
 
 #include "ConsoleGame.hpp"
 
+#include <emscripten/bind.h>
+#include <emscripten.h>
+
+using namespace emscripten;
+
+// external function
+// extern "C"
+// {
+// 	extern void test_js(int number);
+// }
+
+// Do not include main loop if emscripten is used to compile
+#ifndef __EMSCRIPTEN__
+
 int main()
 {
-		for (int ii = 0; ii < 1; ++ii)
+	for (int ii = 0; ii < 1; ++ii)
 	{
 		game::AbstractGame *consoleGame = new game::ConsoleGame();
 
@@ -14,4 +28,24 @@ int main()
 	}
 
 	return EXIT_SUCCESS;
+}
+
+#endif
+
+int main_emscripten(int test)
+{
+	std::cout << test << std::endl;
+
+	// test_js(4242);
+	EM_ASM_ARGS({
+		test_js($0);
+	},
+				42);
+
+	return 42;
+}
+
+EMSCRIPTEN_BINDINGS(module_test)
+{
+	function("main_emscripten", &main_emscripten);
 }
