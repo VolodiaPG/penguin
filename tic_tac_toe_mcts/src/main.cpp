3a12222886de69fef4e767e39abd02ab582e651c
VolodiaPG <55993206+VolodiaPG@users.noreply.github.com>
2019-11-26 19:41:57 +0000

    Removed dead code

--- tic_tac_toe_mcts/src/main.cpp
+++ tic_tac_toe_mcts/src/main.cpp
@@ -2,18 +2,6 @@
 
 #include "ConsoleGame.hpp"
 
-#define PAUSE_TEXT "Please press any key to continue."
-#define pause_msg() pause(PAUSE_TEXT)
-
-void pause(const char *msg)
-{
-	if (msg)
-	{
-		std::cout << msg << std::endl;
-	}
-	std::cin.get();
-}
-
 int main()
 {
 		for (int ii = 0; ii < 1; ++ii)
