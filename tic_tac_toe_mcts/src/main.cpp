aab290a63d1d2104f9bf874dcbdf1bf0b1f7593c
magrimau <47553450+magrimau@users.noreply.github.com>
2019-12-02 16:55:06 +0100

    Command line

--- tic_tac_toe_mcts/src/main.cpp
+++ tic_tac_toe_mcts/src/main.cpp
@@ -94,4 +94,6 @@ extern "C"
 Commande pour build mon-site.html :
 
 em++ src/main.cpp -std=c++17 -o morpion.html -s NO_EXIT_RUNTIME=1 -s EXPORTED_FUNCTIONS="['_play', '_main']" -s EXTRA_EXPORTED_RUNTIME_METHODS="['cwrap', 'ccall']"
+
+em++ src/AbstractBoard.cpp src/AbstractBoardCell.cpp src/AbstractGame.cpp src/AbstractInterface.cpp src/AbstractPlayer.cpp src/Board.cpp src/BoardCell.cpp src/ConsoleGame.cpp src/log.cpp src/main.cpp src/Node.cpp src/Player.cpp src/PlayerVComputer.cpp src/Position.cpp src/TicTacToe.cpp src/Tree.cpp src/TreeVisualizer.cpp -std=c++17 -o www/main.html --shell-file www/tic-tac-toe.html -s ASSERTIONS=2 -s WASM=1 -s NO_EXIT_RUNTIME=1 -s EXPORTED_FUNCTIONS="['_main', '_initGame', '_deleteGame' ,'_play', '_mctsResult', '_checkStatus', '_getPlayerToPlay']" -s EXTRA_EXPORTED_RUNTIME_METHODS="['cwrap', 'ccall']"
 */
