65239c49060b6e1a14b7fd65110bb7b52b049a23
VolodiaPG <55993206+VolodiaPG@users.noreply.github.com>
2019-12-03 09:54:14 +0000

    Green Victory mark
    Co-authored-by: magrimau <maxime.grimaud38@gmail.com>

--- tic_tac_toe_mcts/www/tic-tac-toe.js
+++ tic_tac_toe_mcts/www/tic-tac-toe.js
@@ -57,42 +57,42 @@ var initWrappers = () =>
     * Size of the board
     */
     var N_SIZE = 3;
-    
+
     /**
      * First turn
      */
     var turn = 'X';
-    
+
     /**
      * defines an empty cell
      */
     var EMPTY = '&nbsp;';
-    
+
     /**
      * List of cells
      */
     var cells = [];
-    
+
     /**
      * Transforms a turn into a status
      * @param {string} turn the turn : 'X' or 'O'
      * @returns {number} the turn
      */
     var statusFromTurn = (turn) => turn === 'X' ? 1 : 2;
-    
+
     /**
      * Transforms the status into a turn
      * @param {number} status the status
      * @returns {string} the turn
      */
     var turnFromStatus = (status) => status === 1 ? 'X' : 'O';
-    
+
     init = () =>
     {
         var board = document.createElement('table');
         board.setAttribute('border', 1);
         board.setAttribute('cellspacing', 0);
-    
+
         var identifier = 0;
         for (var i = 0; i < N_SIZE; i++)
         {
@@ -107,7 +107,7 @@ var initWrappers = () =>
                 td.setAttribute('width', 120);
                 td.setAttribute('align', 'center');
                 td.setAttribute('valign', 'center');
-                
+
                 td.classList.add('cell');
                 cell.classList.add('cell-child');
                 content.classList.add('cell-child-content');
@@ -128,12 +128,12 @@ var initWrappers = () =>
                 cell.appendChild(content);
                 td.appendChild(cell);
                 row.appendChild(td);
-    
+
                 identifier++;
-    
+
             }
         }
-    
+
         document.getElementById('tictactoe').appendChild(board);
         startNewGame();
     }
@@ -146,11 +146,12 @@ var initWrappers = () =>
         cells.forEach(el =>
         {
             el.innerHTML = EMPTY;
+            el.classList.remove('highlight-victory');
         });
         document.getElementById('turn').textContent = 'Player ' + turn;
         document.getElementById('hidden').style.display = 'none';
     }
-    
+
     var getCellByIdentifier = (identifier) =>
     {
         for (let ii = 0; ii < cells.length; ++ii)
@@ -158,49 +159,48 @@ var initWrappers = () =>
             if (parseInt(cells[ii].getAttribute('identifier')) === parseInt(identifier))
             {
                 console.log(cells[ii]);
-    
+
                 return cells[ii];
             }
         }
         return undefined;
     }
-    
+
     var setOnEvent = (event) =>
     {
         let identifier = event.srcElement.getAttribute('identifier');
         console.log(`Clicked cell #${identifier}`);
-    
+
         set(identifier);
     }
-    
+
     var set = (identifier) =>
     {
         var self = getCellByIdentifier(identifier);
-    
+
         if (!self || self.innerHTML !== EMPTY)
         {
             return;
         }
-    
+
         self.innerHTML = turn;
-    
-    
+
         let row = parseInt(identifier / 3);
         let col = parseInt(identifier % 3);
-    
+
         play(row, col);
-    
+
         // flip the turns
         turn = turn === 'X' ? 'O' : 'X';
         document.getElementById('turn').textContent = 'Player ' + turn;
-    
+
         let status = checkGameStatus();
         console.log(status);
-    
+
         if (status > 0)
         { // A player won
             //alert(`Player ${turnFromStatus(status)} won!`);
-            searchCombinaison();
+            searchCombinaison(turnFromStatus(status));
             document.getElementById('hidden').style.display = 'block';
             document.getElementById('turn').textContent = 'Player ' + turnFromStatus(status) + ' won !';
             document.getElementById('playAgain').onclick = startNewGame;
@@ -218,85 +218,86 @@ var initWrappers = () =>
             {
                 let cellId = mctsResult();
                 console.log(`Advised Cell #${cellId}`);
-    
+
                 set(cellId);
             }
         }
     }
 
     var searchCombinaison = (turn) => 
-    {
+    {        
         if (cells[0].innerHTML == turn &&
             cells[1].innerHTML == turn &&
             cells[2].innerHTML == turn)
-      {
-        cells[0].setAttribute('background-color', "#5CB85C");
-        cells[1].setAttribute('background-color', "#5CB85C");
-        cells[2].setAttribute('background-color', "#5CB85C");
-      }
+        {
+            cells[0].parentElement.classList.add('highlight-victory');
+            cells[1].parentElement.classList.add('highlight-victory');
+            cells[2].parentElement.classList.add('highlight-victory');
+        }
 
-      if (cells[3].innerHTML == turn &&
-          cells[4].innerHTML == turn &&
-          cells[5].innerHTML == turn)
-      {
-        cells[3].setAttribute('background-color', "#5CB85C");
-        cells[4].setAttribute('background-color', "#5CB85C");
-        cells[5].setAttribute('background-color', "#5CB85C");
-      }
+        if (cells[3].innerHTML == turn &&
+            cells[4].innerHTML == turn &&
+            cells[5].innerHTML == turn)
+        {
+            cells[3].parentElement.classList.add('highlight-victory');
+            cells[4].parentElement.classList.add('highlight-victory');
+            cells[5].parentElement.classList.add('highlight-victory');
+        }
 
-      if (cells[6].innerHTML == turn &&
-          cells[7].innerHTML == turn &&
-          cells[8].innerHTML == turn)
-      {
-        cells[6].setAttribute('background-color', "#5CB85C");
-        cells[7].setAttribute('background-color', "#5CB85C");
-        cells[8].setAttribute('background-color', "#5CB85C");
-      }
+        if (cells[6].innerHTML == turn &&
+            cells[7].innerHTML == turn &&
+            cells[8].innerHTML == turn)
+        {
+            cells[6].parentElement.classList.add('highlight-victory');
+            cells[7].parentElement.classList.add('highlight-victory');
+            cells[8].parentElement.classList.add('highlight-victory');
+        }
 
-      if (cells[0].innerHTML == turn &&
-          cells[3].innerHTML == turn &&
-          cells[6].innerHTML == turn)
-      {
-        cells[0].setAttribute('background-color', "#5CB85C");
-        cells[3].setAttribute('background-color', "#5CB85C");
-        cells[6].setAttribute('background-color', "#5CB85C");
-      }
+        if (cells[0].innerHTML == turn &&
+            cells[3].innerHTML == turn &&
+            cells[6].innerHTML == turn)
+        {
+            cells[0].parentElement.classList.add('highlight-victory');
+            cells[3].parentElement.classList.add('highlight-victory');
+            cells[6].parentElement.classList.add('highlight-victory');
+        }
 
-      if (cells[1].innerHTML == turn &&
-          cells[4].innerHTML == turn &&
-          cells[7].innerHTML == turn)
-      {
-        cells[1].setAttribute('background-color', "#5CB85C");
-        cells[4].setAttribute('background-color', "#5CB85C");
-        cells[7].setAttribute('background-color', "#5CB85C");
-      }
+        if (cells[1].innerHTML == turn &&
+            cells[4].innerHTML == turn &&
+            cells[7].innerHTML == turn)
+        {
+            cells[1].parentElement.classList.add('highlight-victory');
+            cells[4].parentElement.classList.add('highlight-victory');
+            cells[7].parentElement.classList.add('highlight-victory');
+        }
 
-      if (cells[2].innerHTML == turn &&
-          cells[5].innerHTML == turn &&
-          cells[8].innerHTML == turn)
-      {
-        cells[2].setAttribute('background-color', "#5CB85C");
-        cells[5].setAttribute('background-color', "#5CB85C");
-        cells[8].setAttribute('background-color', "#5CB85C");
-      }
+        if (cells[2].innerHTML == turn &&
+            cells[5].innerHTML == turn &&
+            cells[8].innerHTML == turn)
+        {
+            cells[2].parentElement.classList.add('highlight-victory');
+            cells[5].parentElement.classList.add('highlight-victory');
+            cells[8].parentElement.classList.add('highlight-victory');
+        }
 
-      if (cells[0].innerHTML == turn &&
-          cells[4].innerHTML == turn &&
-          cells[8].innerHTML == turn)
-      {
-        cells[0].setAttribute('background-color', "#5CB85C");
-        cells[4].setAttribute('background-color', "#5CB85C");
-        cells[8].setAttribute('background-color', "#5CB85C");
-      }
+        if (cells[0].innerHTML == turn &&
+            cells[4].innerHTML == turn &&
+            cells[8].innerHTML == turn)
+        {
+            cells[0].parentElement.classList.add('highlight-victory');
+            cells[4].parentElement.classList.add('highlight-victory');
+            cells[8].parentElement.classList.add('highlight-victory');
+        }
 
-      if (cells[2].innerHTML == turn &&
-          cells[4].innerHTML == turn &&
-          cells[6].innerHTML == turn)
-      {
-        cells[2].setAttribute('background-color', "#5CB85C");
-        cells[4].setAttribute('background-color', "#5CB85C");
-        cells[6].setAttribute('background-color', "#5CB85C");
-      }
+        if (cells[2].innerHTML == turn &&
+            cells[4].innerHTML == turn &&
+            cells[6].innerHTML == turn)
+        {
+            cells[2].parentElement.classList.add('highlight-victory');
+            cells[4].parentElement.classList.add('highlight-victory');
+            cells[6].parentElement.classList.add('highlight-victory');
+        }
+        multipleVictory.update();
     }
 }
 
