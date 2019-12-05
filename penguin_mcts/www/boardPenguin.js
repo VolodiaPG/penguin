f6142a6ed46922c834621f9cbe9a545261c72314
magrimau <47553450+magrimau@users.noreply.github.com>
2019-12-05 22:45:39 +0100

    All possibles moves

--- penguin_mcts/www/boardPenguin.js
+++ penguin_mcts/www/boardPenguin.js
@@ -5,14 +5,45 @@
     var hp = window.HexPixi;
     var map = null,
         stage = new hp.PIXI.Stage(0xe0e0e0),
-        renderer = new hp.PIXI.autoDetectRenderer(1000, 500, { //555, 440
+        renderer = new hp.PIXI.autoDetectRenderer(555, 440, { //555, 440
             antialiasing: false,
             transparent: false,
             resolution: 1
         });
 
     function onHexClick(m, cell) {
+        var r, c;
+        console.log("Cell selected : (" + cell.row + "," + cell.column + ")");
+        
+        for(c = 0; c < map.cells[cell.row].length ; c+=1) {
+            // console.log("Cell row : (" + map.cells[cell.row][c].row + "," + map.cells[cell.row][c].column + ")")
+            map.setCellTerrainType(map.cells[cell.row][c], 1);
+        }
+
+        //diago droite
+        var cellX = cell.column - (cell.row + (cell.row & 1)) / 2;
+
+        //ligne
+        var cellZ = cell.row;
+
+        //diago gauche
+        var cellY = - cellX - cellZ;
+
+        for(var r = 0; r < map.cells.length ; r+=1) {
+            for(var c = 0 ; c < map.cells[r].length ; c+=1) {
+                var tempX = map.cells[r][c].column - (map.cells[r][c].row + (map.cells[r][c].row & 1)) / 2;
+                var tempZ = map.cells[r][c].row;
+                var tempY = - tempX - tempZ;
+
+                if ((tempX == cellX) || (tempY == cellY) || (tempZ == cellZ)){
+                    map.setCellTerrainType(map.cells[r][c], 1);
+                }
+
+            }
+        }
+
         map.setCellTerrainType(cell, 0);
+
     }
 
     function animate() {
@@ -34,12 +65,13 @@
             hexBottomPad: 24,
             onHexClick: onHexClick,
             textures: [
-                "images/game/tileSnow.png",
-                "images/game/tileWater_full.png"
+                "images/game/tileStone_full.png",
+                "images/game/tileSnow.png"               
             ],
             terrainTypes: [
                 { name: "empty", color: 0xffffff, isEmpty: true },
-                { name: "snow", tileIndex: 0, color: 0xe2e2fa }
+                { name: "stone", tileIndex: 0, color: 0x808080 },
+                { name: "snow", tileIndex: 1, color: 0xe2e2fa }
             ],
             onAssetsLoaded: function () { requestAnimFrame(animate); }
         };
@@ -55,9 +87,15 @@
 
     function initPage() {
         setupPixiJs();
-        map.generateMap(1);
+        map.generateMap(2);
 
-        console.log(map.cells[4].length);
+        console.log("Ordre du tableau : ");
+
+        for(var r = 0; r < map.cells.length ; r+=1) {
+            for(var c = 0 ; c < map.cells[r].length ; c+=1) {
+                console.log("Cell : (" + map.cells[r][c].row + "," + map.cells[r][c].column + ")");
+            }
+        }
     }
 
     window.requestAnimFrame = (function(callback) {
