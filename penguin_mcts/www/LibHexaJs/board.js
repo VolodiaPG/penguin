1724e9e65fdebde1fa24a10c40dcf81d35c15748
MaximeGrmd <maxime.grimaud38@gmail.com>
2019-12-13 14:41:13 +0100

    Constructor Board

--- penguin_mcts/www/LibHexaJs/board.js
+++ penguin_mcts/www/LibHexaJs/board.js
@@ -4,10 +4,6 @@
 
  export class Board {
      constructor() {
-        this.mapWidth = 8;
-        this.mapHeight = 8;
-        
-        this.textures = [];
         this.hexes = new PIXI.Graphics();
         this.container = new PIXI.Container();
         this.pixiStage = null;
@@ -25,6 +21,48 @@
         this.pixiApp.renderer.view.style.display = "block";
         this.pixiApp.renderer.autoResize = true;
         this.pixiApp.renderer.resize(window.innerWidth, window.innerHeight);
+
+        // The map's number of cells across (cell column count).
+        this.mapWidth = 8;
+        // The map's number of cells high (cell row count).
+        this.mapHeight = 8;
+        // The map's number of cells high (cell row count).
+        this.mapHeight = 10;
+        // The radius of the hex. Ignored if hexWidth and hexHeight are set to non-null.
+        this.hexSize = 40;
+        this.drawHexSize = 40;
+        // The pixel width of a hex.
+        this.hexWidth = 65;
+        // The pixel height of a hex.
+        this.hexHeight = 65;
+        // The color to use when drawing hex outlines.
+        this.hexLineColor = 0xd0d0d0;
+        // The width in pixels of the hex outline.
+        this.hexLineWidth = 2;
+        // Callback function (cell) that handles a hex being clicked on or tapped.
+        this.onHexClick = null;
+        this.onHexHover = null;
+        this.dontBlurryImages = false;
+        this.sizeBasedOnTexture = false;
+        this.offsetX = 0;
+        this.offsetY = 0;
+
+        // Specify the types of terrain available on the map. Map cells reference these terrain
+        // types by index. Add custom properties to extend functionality.
+        this.terrainTypes = [
+            { name: "empty", color: 0xffffff, isEmpty: true },
+            { name: "selected", tileIndex: 0, color: 0x808080 },
+            { name: "snow", tileIndex: 1, color: 0xe2e2fa }
+        ];
+        // Array of strings that specify the url of a texture. Can be referenced by index in terrainType.
+        this.textures = [
+            "images/game/tileWater_full.png",
+            "images/game/tileSnow.png",
+        ];
+
+        // This is the pixel height specifying an area of overlap for hex cells. Necessary when
+        // working with isometric view art systems.
+        this.hexBottomPad = 24;    
 
         this.cells = [];
         this.cellHighlighter = null;
