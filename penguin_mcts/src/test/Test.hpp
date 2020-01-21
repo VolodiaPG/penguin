61c244976f9231907cc33e9a3523b297cd7a394c
VolodiaPG <volodia.parolguarino@gmail.com>
2020-01-21 22:59:15 +0100

    Added test class

---
+++ penguin_mcts/src/test/Test.hpp
@@ -0,0 +1,13 @@
+#ifndef TEST_HPP_
+#define TEST_HPP_
+
+#include <iostream>
+
+class Test{
+    public:
+        void print(){
+            std::cout << "Test" << std::endl;
+        }
+};
+
+#endif
