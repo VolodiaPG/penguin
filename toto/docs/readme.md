621993fd3f1dfb1ac72b273c859a6787e740f7d2
VolodiaPG <volodia.parolguarino@gmail.com>
2020-03-21 17:25:30 +0100

    temp

---
+++ toto/docs/readme.md
@@ -0,0 +1,9 @@
+# penguin_mcts
+
+Welcome to the project documentation!
+
+Use `npm run docs` for easier navigation.
+
+## Available documentation
+
+[[index]]
---
+++ toto/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { TranslateModule } from '@ngx-translate/core';
+import { RouterModule } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+
+import { CoreModule } from '@app/core';
+import { SharedModule } from '@app/shared';
+import { HomeModule } from './home/home.module';
+import { ShellModule } from './shell/shell.module';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    TranslateModule.forRoot(),
+    IonicModule.forRoot(),
+    CoreModule,
+    SharedModule,
+    ShellModule,
+    HomeModule,
+    AppRoutingModule // must be imported as the last module as it contains the fallback route
+  ],
+  declarations: [AppComponent],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
