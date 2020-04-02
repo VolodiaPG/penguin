fb28df1cedfd43d012009f3abc96dcb48a142970
volodiapg <volodia.parolguarino@gmail.com>
2020-04-02 14:54:57 +0000

    Added cubic to offset & fixed bugs with cubic transformation

--- src/game_logic/utils.cpp
+++ src/game_logic/utils.cpp
@@ -4,17 +4,27 @@ namespace game
 {
 Position3D hex_axial_to_cube(const Position &position)
 {
+    int x = position.x;
+    int z = position.y;
+    int y = -x - z;
     return Position3D{
-        position.x,
-        position.y,
-        -position.x - position.y};
+        x,
+        y,
+        z};
 }
 
 Position hex_cube_to_axial(const Position3D &position)
 {
-
     return Position{
         position.x,
         position.z};
+}
+
+Position hex_cube_to_offset(const Position3D &position)
+{
+    return Position{
+        position.x + (position.z - (position.z&1))/2,
+        position.z
+    };
 }
 } // namespace game
