1639680b0f9de6cbfe79731a309f04ea7ac4c90f
volodiapg <volodia.parolguarino@gmail.com>
2020-04-06 17:11:46 +0000

    removed useless bits of code from previous versions

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,13 @@
 #include "game_logic/tic_tac_toe/PlayerVComputer.hpp"
 #include "game_logic/Position.hpp"
 
-#ifdef __EMSCRIPTEN__
-#include <emscripten.h>
-// #include <emscripten/bind.h>
-#endif
+// #ifdef __EMSCRIPTEN__
+// #include <emscripten.h>
+// // #include <emscripten/bind.h>
+// #endif
 
-// using namespace emscripten;
-#ifndef __EMSCRIPTEN__
+// // using namespace emscripten;
+// #ifndef __EMSCRIPTEN__
 int main()
 {	
 	for (int ii = 0; ii < 1; ++ii)
@@ -22,74 +22,6 @@ int main()
 
 	return EXIT_SUCCESS;
 }
-#else
-extern "C"
-{
-	// EMSCRIPTEN_KEEPALIVE
-	game::PlayerVComputer *gameInstance;
-
-	EMSCRIPTEN_KEEPALIVE
-	int main()
-	{
-		std::cout << "Runtime launched" << std::endl;
-		return EXIT_SUCCESS;
-	}
-
-	EMSCRIPTEN_KEEPALIVE
-	int initGame()
-	{
-		gameInstance = new game::PlayerVComputer();
-		std::cout << "Initialization Complete" << std::endl;
-
-		return EXIT_SUCCESS;
-	}
-
-	EMSCRIPTEN_KEEPALIVE
-	void deleteGame()
-	{
-		if (gameInstance)
-			delete gameInstance;
-
-		std::cout << "Free Complete" << std::endl;
-	}
-
-	EMSCRIPTEN_KEEPALIVE
-	int getPlayerToPlay()
-	{
-		return gameInstance->getPlayerToPlay()->getId();
-	}
-
-	EMSCRIPTEN_KEEPALIVE
-	int checkStatus()
-	{
-		return gameInstance->checkStatus();
-	}
-
-	EMSCRIPTEN_KEEPALIVE
-	void play(int row, int col)
-	{
-		//std::cout << "Joué en (" << row << "," << col << ")" << std::endl;
-		gameInstance->play(row, col);
-	}
-
-	EMSCRIPTEN_KEEPALIVE
-	int mctsResult()
-	{
-		std::cout << "Player 2" << std::endl;
-		int ret = -1;
-		game::BoardCell *cell;
-
-		game::AbstractBoardCell *absCell = gameInstance->mctsResult();
-		if ((cell = dynamic_cast<game::BoardCell *>(absCell)) != nullptr)
-		{
-			game::Position pos = cell->getPosition();
-			ret = pos.x * 3 + pos.y;
-		}
-
-		return ret;
-	}
-}
-#endif
 
 /* 
 Commande pour build mon-site.html :
