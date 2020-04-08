0c2fd620d5c3624035ba901a346abd398eaf2985
volodiapg <volodia.parolguarino@gmail.com>
2020-04-08 10:58:29 +0000

    more c++ compliant

--- src/game_logic/utils.cpp
+++ src/game_logic/utils.cpp
@@ -4,27 +4,26 @@ namespace game
 {
 Position3D hex_axial_to_cube(const Position &position)
 {
-    int x = position.x;
-    int z = position.y;
-    int y = -x - z;
-    return Position3D{
-        x,
-        y,
-        z};
+    Position3D ret;
+    ret.x = position.x;
+    ret.z = position.y;
+    ret.y = -ret.x - ret.z;
+    return ret;
 }
 
 Position hex_cube_to_axial(const Position3D &position)
 {
-    return Position{
-        position.x,
-        position.z};
+    Position ret;
+    ret.x = position.x;
+    ret.y = position.z;
+    return ret;
 }
 
 Position hex_cube_to_offset(const Position3D &position)
 {
-    return Position{
-        position.x + (position.z - (position.z&1))/2,
-        position.z
-    };
+    Position ret;
+    ret.x = position.x + (position.z - (position.z&1))/2;
+    ret.y = position.z;
+    return ret;
 }
 } // namespace game
