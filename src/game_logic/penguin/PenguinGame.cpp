9aa552b33854e6c847887fa33bb4404b8471ac71
volodiapg <volodia.parolguarino@gmail.com>
2020-04-09 16:50:42 +0000

    Encapsulation of the game

---
+++ src/game_logic/penguin/PenguinGame.cpp
@@ -0,0 +1,54 @@
+#include "PenguinGame.hpp"
+
+namespace game
+{
+namespace penguin
+{
+
+PenguinGame::PenguinGame(const size_t dimension, const size_t number_of_penguins_per_team)
+    : AbstractGame(new Board(dimension, number_of_penguins_per_team)) // nullptr during construct, then we define the board
+{
+}
+
+PenguinGame::~PenguinGame()
+{
+    delete board;
+}
+
+bool PenguinGame::play(const int player_id, BoardCell *move)
+{
+    ++numberMoves;
+    return board->performMove(player_id, move);
+}
+
+void PenguinGame::revertPlay(BoardCell *cell)
+{
+    --numberMoves;
+    int player = 0;
+
+    if (numberMoves % 2)
+    {
+        player = 1;
+    }
+    
+    board->revertMove(player, cell);
+}
+
+bool PenguinGame::isFinished() const
+{
+    return board->checkStatus() != 0;
+}
+
+int PenguinGame::getPlayerToPlay() const
+{
+    int nextPlayer = 2;
+
+    if (numberMoves % 2)
+    {
+        nextPlayer = 1;
+    }
+
+    return nextPlayer;
+}
+} // namespace tic_tac_toe
+} // namespace game
