c4366c8e463ea7673cecf956d2e0ed48ca035c8b
volodiapg <volodia.parolguarino@gmail.com>
2020-04-15 12:26:17 +0000

    Changed constructor of mcts::Node

--- src/game_logic/tic_tac_toe/PlayerVComputer.cpp
+++ src/game_logic/tic_tac_toe/PlayerVComputer.cpp
@@ -23,20 +23,9 @@ Move PlayerVComputer::mctsResult()
     mcts::MCTSConstraints constraints;
     constraints.time = 250;
     auto game = dynamic_cast<AbstractGame<AbstractBoardCell, AbstractPlayer, AbstractPawn<AbstractPlayer, AbstractBoardCell>> *>(this);
-    mcts::Tree tree(game, board->getPlayerById(getPlayerToPlay()), constraints);
+    mcts::Tree tree(game, constraints);
     tree.begin();
     return tree.bestMove();
 }
 } // namespace tic_tac_toe
 } // namespace game
-
-// EMSCRIPTEN_BINDINGS(module_player_v_computer)
-// {
-//     // function("playPlayer1", &playPlayer1);
-//     // function("playPlayer2", &playPlayer2);
-//     class_<game::PlayerVComputer>("PlayerVComputer")
-//         .constructor<>()
-//         .function("playPlayer1", &game::PlayerVComputer::playPlayer1)
-//         .function("playPlayer2", &game::PlayerVComputer::playPlayer2)
-//         .function("isFinished", &game::PlayerVComputer::isFinished);
-// }
