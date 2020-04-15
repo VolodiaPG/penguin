65f1b379c0459ba2d0ef3ab25457dc75b3358306
volodiapg <volodia.parolguarino@gmail.com>
2020-04-15 12:26:46 +0000

    added message + changed to test the penguin implementation

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,25 @@
 #include "game_logic/penguin/ConsoleGame.hpp"
 #include "game_logic/tic_tac_toe/ConsoleGame.hpp"
 
+#ifndef NDEBUG
+#pragma message("WARNING: `NDEBUG` is not defined, enabling all the debug tools and checks present (assert, etc.)") 
+#endif
+
 #ifndef __EMSCRIPTEN__
 int main()
 {	
-	// for (int ii = 0; ii < 1; ++ii)
-	// {
-	// 	game::penguin::ConsoleGame consoleGame;
-
-	// 	consoleGame.loop();
-	// }
 	for (int ii = 0; ii < 1; ++ii)
 	{
-		game::tic_tac_toe::ConsoleGame consoleGame;
+		game::penguin::ConsoleGame consoleGame;
 
 		consoleGame.loop();
 	}
+	// for (int ii = 0; ii < 1; ++ii)
+	// {
+	// 	game::tic_tac_toe::ConsoleGame consoleGame;
+
+	// 	consoleGame.loop();
+	// }
 
 	return EXIT_SUCCESS;
 }
