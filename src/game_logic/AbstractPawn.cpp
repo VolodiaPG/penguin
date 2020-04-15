45253a2fccc6e67d169efd8c401556b7c78c85db
volodiapg <volodia.parolguarino@gmail.com>
2020-04-15 13:21:43 +0000

    identing

--- src/game_logic/AbstractPawn.cpp
+++ src/game_logic/AbstractPawn.cpp
@@ -11,7 +11,6 @@
 #include "penguin/HumanPlayer.hpp"
 #include "penguin/BoardCell.hpp"
 
-
 namespace game
 {
 
@@ -26,17 +25,19 @@ AbstractPawn<PlayerT, CellT>::AbstractPawn(unsigned int id, PlayerT *owner)
 template <class PlayerT, class CellT>
 void AbstractPawn<PlayerT, CellT>::makeMove(CellT *cell)
 {
-    _owner->_moves_done.push({(AbstractBoardCell*)_current_cell, (AbstractBoardCell *)cell, (AbstractPawn<game::AbstractPlayer, game::AbstractBoardCell> *)this});
+    _owner->_moves_done.push({(AbstractBoardCell *)_current_cell,
+                              (AbstractBoardCell *)cell,
+                              (AbstractPawn<game::AbstractPlayer, game::AbstractBoardCell> *)this});
     _current_cell = cell;
 }
 
 template <class PlayerT, class CellT>
 CellT *AbstractPawn<PlayerT, CellT>::getCurrentCell() const
 {
-    return  _current_cell;
+    return _current_cell;
 }
 
-template class game::AbstractPawn<AbstractPlayer, AbstractBoardCell>;
+template class AbstractPawn<AbstractPlayer, AbstractBoardCell>;
 template class AbstractPawn<tic_tac_toe::Player, tic_tac_toe::BoardCell>;
 template class AbstractPawn<penguin::HumanPlayer, penguin::BoardCell>;
 } // namespace game
