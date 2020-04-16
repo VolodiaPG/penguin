d13550a12aad8c5c366fa85d956df95b3083716f
MaximeGrmd <47553450+MaximeGrmd@users.noreply.github.com>
2020-04-16 10:27:02 +0000

    penguin animation translateX and translateY

--- www/src/app/game/board/penguin/penguin.component.ts
+++ www/src/app/game/board/penguin/penguin.component.ts
@@ -11,17 +11,12 @@ export class PenguinComponent implements OnInit {
 
   position: Pos;
 
-  startPoint: number;
-  endPoint:number;
-
   moveToAnimation:any;
 
   constructor() { }
 
   ngOnInit() {
     this.position = new Pos(0,0);
-    this.startPoint = this.position.x;
-    this.endPoint = this.position.x;
   }
 
   /***************************************************************************************************************************
@@ -30,30 +25,16 @@ export class PenguinComponent implements OnInit {
   onPenguinClick() {
     console.log("Penguin clicked");
 
-    this.startPoint = this.position.x;
-    this.position.x = this.position.x + 90;
-    this.endPoint = this.position.x;
+    this.position.x = this.position.x + 180;
+    this.position.y = this.position.y + 450;
 
     this.moveToAnimation = anime({
       targets: '.penguin',
-      translateX: this.endPoint,
+      translateX: this.position.x,
+      translateY: this.position.y,
       duration: 2000,
-      property: [this.startPoint, this.endPoint],
+      property: [this.position],
     });
-
-    console.log(this.startPoint + "->" + this.endPoint);
-
-    // anime({
-    //   targets: '.penguin',
-    //   translateX: function(el:any) {
-    //     el.setAttribute('data-x', el.getAttribute('data-x') + 90)
-    //     return el.getAttribute('data-x');
-    //   },
-    //   rotate: '1turn',
-    //   duration: 2000,
-    //   loop: true
-    // });
-
   }
 
 }
