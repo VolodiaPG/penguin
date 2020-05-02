33aac99686fcdeaa6c6366aa53c9d021127f5e09
volodiapg <volodia.parolguarino@gmail.com>
2020-05-02 16:39:05 +0000

    added bindings for Move

--- src/game_logic/utils/Move.cpp
+++ src/game_logic/utils/Move.cpp
@@ -8,6 +8,23 @@
 
 namespace game
 {
+
+template <class CellT, class PawnT>
+bool Move<CellT, PawnT>::equals_to(const Move &move) const
+{
+    bool ret = true;
+    if (move.from != from)
+        ret = from != nullptr && move.from != nullptr && *move.from == *from;
+
+    if (ret && move.target != target)
+        ret = target != nullptr && move.target != nullptr && *move.target == *target;
+
+    if (ret && move.pawn != pawn)
+        ret = pawn != nullptr && move.pawn != nullptr && *move.pawn == *pawn;
+
+    return ret;
+}
+
 template struct Move<tic_tac_toe::BoardCell, tic_tac_toe::Player>;
 template struct Move<penguin::BoardCell, penguin::PenguinPawn>;
 } // namespace game
