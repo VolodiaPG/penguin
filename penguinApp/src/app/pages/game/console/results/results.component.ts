48c88fe805549b08c2bbd74ec252903fcedb9772
volodiapg <volodia.parolguarino@gmail.com>
2020-05-25 11:28:52 +0000

    2 lines

--- penguinApp/src/app/pages/game/console/results/results.component.ts
+++ penguinApp/src/app/pages/game/console/results/results.component.ts
@@ -19,14 +19,14 @@ export class ResultsComponent implements OnInit {
   private mctsRatio: number;
   private humanRatio: number;
 
-  constructor(private consoleService: ConsoleService {
+  constructor(private consoleService: ConsoleService) {
     this.subscription = this.consoleService.getResults().subscribe(res => {
-      if(res) {
-        this.mctsRatio = res.score/res.visits;
+      if (res) {
+        this.mctsRatio = res.score / res.visits;
         this.humanRatio = 1 - this.mctsRatio;
       }
     });
-   }
+  }
 
   ngOnInit() {
     this.chart = new Chart(this.resultsCanvas.nativeElement, {
@@ -39,16 +39,12 @@ export class ResultsComponent implements OnInit {
             data: [12, 19, 3, 5, 2, 3],
             backgroundColor: [
               "rgba(255, 99, 132, 0.2)",
-              "rgba(54, 162, 235, 0.2)",
-              "rgba(255, 206, 86, 0.2)",
-              "rgba(75, 192, 192, 0.2)",
-              "rgba(153, 102, 255, 0.2)",
-              "rgba(255, 159, 64, 0.2)"
+              "rgba(54, 162, 235, 0.2)"
             ],
-            hoverBackgroundColor: ["#FF6384", "#36A2EB", "#FFCE56", "#FF6384", "#36A2EB", "#FFCE56"]
+            hoverBackgroundColor: ["#FF6384", "#36A2EB"]
           }
         ]
       }
-    }); 
+    });
   }
 }
