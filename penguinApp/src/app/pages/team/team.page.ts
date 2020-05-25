f0d453dd0bb02950d7b35c4169a67af2f29b662a
volodiapg <volodia.parolguarino@gmail.com>
2020-05-25 07:36:57 +0000

    Added Team page

--- penguinApp/src/app/pages/team/team.page.ts
+++ penguinApp/src/app/pages/team/team.page.ts
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeamPage implements OnInit {
 
-  constructor() { }
+  teamMembers: Array<{ name: string; role: string; description: string }> = [
+    {
+      name: 'Volodia',
+      role: 'C++ / MCTS / WebAssembly',
+      description: 'Penguin'
+    },
+    {
+      name: 'Maxime',
+      role: 'Graphical designer / WebAssembly',
+      description: 'Penguin'
+    },
+    {
+      name: 'Romain Hubert',
+      role: 'C++ / MCTS',
+      description: 'Penguin'
+    },
+    {
+      name: 'Romain Hu',
+      role: 'Graphical desginer',
+      description: 'Penguin'
+    },
+    {
+      name: 'Clément',
+      role: 'Graphical designer',
+      description: 'Penguin'
+    }
+  ];
 
-  ngOnInit() {
-  }
+  constructor() {}
+
+  ngOnInit() {}
 
 }
