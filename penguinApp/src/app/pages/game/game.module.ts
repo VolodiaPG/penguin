bbcbb49a97aca0c76d562c8c791218277de187cb
MaximeGrmd <47553450+MaximeGrmd@users.noreply.github.com>
2020-05-25 17:00:48 +0200

    add graph

--- penguinApp/src/app/pages/game/game.module.ts
+++ penguinApp/src/app/pages/game/game.module.ts
@@ -11,17 +11,20 @@ import { GamePage } from './game.page';
 import { BoardComponent } from './board/board.component';
 import { ControlComponent } from './control/control.component';
 import { ConsoleComponent } from './console/console.component';
-import { ResultsComponent } from './console/results/results.component';
+import { ResultsComponent } from './results/results.component';
 import { HexComponent } from './board/hex/hex.component';
 import { PenguinComponent } from './board/penguin/penguin.component';
 import { InfoComponent } from './info/info.component';
+
+import { ChartsModule } from 'ng2-charts';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    GamePageRoutingModule
+    GamePageRoutingModule,
+    ChartsModule
   ],
   declarations: [
     GamePage,
